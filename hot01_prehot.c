e47c2feac7f02cd20640e4fa24693b3fcc05bb20
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-08-29 08:49:52 +0800

    [HOT1: Prehot] Update variable
    Continue updating variable name.

--- hot01_prehot.c
+++ hot01_prehot.c
@@ -2,8 +2,8 @@
 
 int main(void){
 
-    char staffName[20], staffID[20], staffPosition[5];
-    float markT, markS, markP, markO;
+    char staffName[20], staffID[20], staffPosition[5], grade[5];
+    float markT, markS, markP, markO, markTotal;
 
     printf("\t\tWELCOME TO UMG KPI CALCULATION");
     printf("\n______________________");
@@ -17,33 +17,33 @@ int main(void){
     printf("\n\nEnter your name                           : ");
     gets(staffName);
 
-    printf("\nEnter your staff ID                       : ");
+    printf("Enter your staff ID                       : ");
     gets(staffID);
 
-    printf("\nPosition, P (Permanent) or T (Trial)      : ");
+    printf("Position, P (Permanent) or T (Trial)      : ");
     scanf(" %s", &staffPosition);
 
-    printf("\nEnter your marks for Teaching    (0-25)   : ");
+    printf("Enter your marks for Teaching    (0-25)   : ");
     scanf(" %f", &markT);
 
-    printf("\nEnter your marks for Supervision (0-25)   : ");
+    printf("Enter your marks for Supervision (0-25)   : ");
     scanf(" %f", &markS);
 
-    printf("\nEnter your marks for Publication (0-25)   : ");
+    printf("Enter your marks for Publication (0-25)   : ");
     scanf(" %f", &markP);
 
-    printf("\nEnter your marks for Others      (0-25)   : ");
+    printf("Enter your marks for Others      (0-25)   : ");
     scanf(" %f", &markO);
 
     printf("\n\nERROR: Mark exceed 25\n***Please try again\n\n");
 
     printf("\n________________________________________________________________");
-    printf("\nName                                      : ");
-    printf("\nStaff ID                                  : ");
-    printf("\nPosition                                  : ");
+    printf("\nName                                      : %s", staffName);
+    printf("\nStaff ID                                  : %s", staffID);
+    printf("\nPosition                                  : %s", staffPosition);
     printf("\n________________________________________________________________");
-    printf("\nTotal Marks                               : ");
-    printf("\nGrade                                     : ");
+    printf("\nTotal Marks                               : %.2f", markTotal);
+    printf("\nGrade                                     : %s", grade);
 
     printf("\n");
 
