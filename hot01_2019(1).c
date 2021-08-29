8b578339299f0e89e53583a3475d6cf07045d365
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-08-29 18:46:05 +0800

    Create hot01_2019(1).c
    Expected output as the main structure.

---
+++ hot01_2019(1).c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("BUKIT GAMBANG WATER PARK RESERVATION SYSTEM");
+    printf("\nPackage 1: 1 Day Water Park");
+    printf("\nPackage 2: 1 Day Theme Park");
+    printf("\nPackage 3: 1 Day Combo (Theme Park + Water Park)");
+    printf("\n---------------------------------------------------");
+
+    printf("\nInsert customer's name: ");
+
+    printf("\nInsert customer's phone number: ");
+
+    printf("\nInsert customer's email: ");
+
+    printf("\nChoose a package (1/2/3): ");
+
+    printf("\nNumber of person (Adult): ");
+
+    printf("\nNumber of person (Child): ");
+
+    printf("\nChoose membership card (G=Gold, S=Silver, P=Platinum, N=None): ");
+
+    printf("\n\nTicket Info:");
+    printf("\n-----------------------------");
+    printf("\nName: ");
+    printf("\nPhone Number: ");
+    printf("\nPackage: ");
+    printf("\nTicket Price: RM");
+
+    printf("\n\nExit system? (y/n): ");
+
+    printf("\n\nTotal Ticket Sales : RM \n");
+
+return 0;
+}
