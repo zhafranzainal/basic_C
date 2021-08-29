86abe91555e98604204c1541a878a1e292e9ec67
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-08-29 08:36:22 +0800

    [HOT1: Prehot] Create hot01_prehot.c
    Expected output as the main structure.

---
+++ hot01_prehot.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("\t\tWELCOME TO UMG KPI CALCULATION");
+    printf("\n______________________");
+    printf("\nINDICATOR:        ");
+    printf("\nA = Excellent     ");
+    printf("\nB = Good          ");
+    printf("\nC = Moderate      ");
+    printf("\nF = Failed        ");
+    printf("\n______________________");
+
+    printf("\n\nEnter your name                           : ");
+    printf("\nEnter your staff ID                       : ");
+    printf("\nPosition, P (Permanent) or T (Trial)      : ");
+    printf("\nEnter your marks for Teaching    (0-25)   : ");
+    printf("\nEnter your marks for Supervision (0-25)   : ");
+    printf("\nEnter your marks for Publication (0-25)   : ");
+    printf("\nEnter your marks for Others      (0-25)   : ");
+
+    printf("\n\nERROR: Mark exceed 25\n***Please try again\n\n");
+
+    printf("\n________________________________________________________________");
+    printf("\nName                                      : ");
+    printf("\nStaff ID                                  : ");
+    printf("\nPosition                                  : ");
+    printf("\n________________________________________________________________");
+    printf("\nTotal Marks                               : ");
+    printf("\nGrade                                     : ");
+
+    printf("\n");
+
+return 0;
+}
