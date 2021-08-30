8b08213ef7eda035b3337c30750c3d467ee4a945
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-08-30 14:33:49 +0800

    [HOT1: 2019(2)] Create hot01_2019(2).c
    Expected output as the main structure.

---
+++ hot01_2019(2).c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Personal Information");
+    printf("\n--------------------");
+
+    printf("\nName: ");
+
+    printf("\nIdentification Card: ");
+
+    printf("\nAddress: ");
+
+    printf("\n\nHousehold Income");
+    printf("\n----------------");
+    printf("\nBasic Salary  : RM ");
+
+    printf("\nAllowance     : RM ");
+
+    printf("\nSide Income   : RM ");
+
+    printf("\n\nHousehold income from spouse? [y/n]: ");
+
+    printf("\nBasic Salary (Spouse): RM ");
+
+    printf("\nAllowance    (Spouse): RM ");
+
+    printf("\nSide Income  (Spouse): RM ");
+
+    printf("\n\n-------------------------------");
+    printf("\nName: ");
+    printf("\nTotal household income is RM ");
+    printf("\nYou are in T20 group.");
+    printf("\nTax to pay: RM ");
+
+    printf("\n-------------------------------");
+    printf("\nExit Program? [y/n]: ");
+
+    printf("\n");
+
+return 0;
+}
