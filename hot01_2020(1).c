5b36a36fc0c415ff0d582be528e15ebefd979364
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-08-31 20:15:21 +0800

    [HOT1: 2020(1)] Determine data type and variable name
    Determine data type and variable name before variable declaration.

--- hot01_2020(1).c
+++ hot01_2020(1).c
@@ -3,18 +3,21 @@
 int main(void){
 
     printf("Enter Name: ");
+    gets(custName);
 
-    printf("\nEnter Contact Number: ");
+    printf("Enter Contact Number: ");
+    gets(custNum);
 
-    printf("\n\nWelcome Jeffy to Berjaya Tioman Tourism.");
+    printf("\nWelcome %s to Berjaya Tioman Tourism.", custName);
     printf("\n---------------------------------------");
     printf("\nTRIP A - Round Tioman Island (6 sites)");
     printf("\nTRIP B - Coral Island/Snorkeling Tour (4 sites)");
     printf("\nTRIP C - Fishing & Snorkeling\n");
 
     printf("\nTrip Package [A/B/C]: ");
+    scanf(" %s", &tripPackage);
 
-    printf("\n----------------------------");
+    printf("----------------------------");
 
     printf("\nTRIP A - Round Tioman Island");
     printf("\nVisit SIX (6) sites starts from Mukut Waterfall, Juara Village, Deep Bay, Reef Shark, Marine Park and Renggis Island.");
@@ -26,13 +29,15 @@ int main(void){
     printf("\nEnjoy our fishing exploration at Malang Rock and Renggis Island.");
 
     printf("\n\nNo of person (4-12 pax): ");
+    scanf( "%d", &custPax);
 
-    printf("\nYour trip price for 10 PAX: RM ");
+    printf("Your trip price for %d PAX: RM %.2f", custPax, tripPrice);
 
     printf("\n\nCheck for other trip packages? [y/n]: ");
+    scanf(" %s", &userExit);
 
-    printf("\n\nEnjoy the trip!");
-    printf("\nTotal to pay is: RM \n");
+    printf("\nEnjoy the trip!");
+    printf("\nTotal to pay is: RM %.2f\n", tripPay);
 
 return 0;
 }
