687c3bbe1221861bfac7d0d8320208c0cdde2495
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-08-31 20:04:42 +0800

    [HOT1: 2020(1)] Created hot01_2020(1).c
    Expected output as the main structure.

---
+++ hot01_2020(1).c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Enter Name: ");
+
+    printf("\nEnter Contact Number: ");
+
+    printf("\n\nWelcome Jeffy to Berjaya Tioman Tourism.");
+    printf("\n---------------------------------------");
+    printf("\nTRIP A - Round Tioman Island (6 sites)");
+    printf("\nTRIP B - Coral Island/Snorkeling Tour (4 sites)");
+    printf("\nTRIP C - Fishing & Snorkeling\n");
+
+    printf("\nTrip Package [A/B/C]: ");
+
+    printf("\n----------------------------");
+
+    printf("\nTRIP A - Round Tioman Island");
+    printf("\nVisit SIX (6) sites starts from Mukut Waterfall, Juara Village, Deep Bay, Reef Shark, Marine Park and Renggis Island.");
+
+    printf("\nTRIP B - Coral Island/Snorkeling Tour");
+    printf("\nVisit FOUR (4) sites starts from Coral Island, Malang Rock, Marine Park and Renggis Island.");
+
+    printf("\nTRIP C - Fishing & Snorkeling");
+    printf("\nEnjoy our fishing exploration at Malang Rock and Renggis Island.");
+
+    printf("\n\nNo of person (4-12 pax): ");
+
+    printf("\nYour trip price for 10 PAX: RM ");
+
+    printf("\n\nCheck for other trip packages? [y/n]: ");
+
+    printf("\n\nEnjoy the trip!");
+    printf("\nTotal to pay is: RM \n");
+
+return 0;
+}
