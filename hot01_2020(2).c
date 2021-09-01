fb46b5afeb64302be70d2ce711412666909ce88c
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-09-01 18:58:37 +0800

    [HOT1: 2020(2)] Create hot01_2020(2).c
    Expected output as the main structure.

---
+++ hot01_2020(2).c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Welcome to Nasi Kukus MyAbah Restaurant");
+
+    printf("\n---------------------------------------");
+    printf("\n **MENU**");
+    printf("\n AR - Nasi + Ayam goreng berempah");
+    printf("\n AC - Nasi + Ayam masak kicap");
+    printf("\n AK - Nasi + Ayam masak kari");
+    printf("\n AH - Nasi + Ayam masak merah");
+    printf("\n AP - Nasi + Ayam percik");
+    printf("\n AM - Nasi + Ayam madu");
+
+    printf("\n\n **DRINK**");
+    printf("\n DSL - Sirap Limau");
+    printf("\n DTL - Teh O Limau");
+    printf("\n DSR - Sarsi");
+    printf("\n DAM - Air Mineral");
+
+    printf("\n---------------------------------------\n");
+
+    printf("\nEnter name                            : ");
+
+    printf("\nNumber of order                       : ");
+
+    printf("\nOrder type? (1=Dine in, 0=Takeaway)   : ");
+
+    printf("\nNeed plastic bag? (1=Yes, 0=No)       : ");
+
+    printf("\n");
+
+    printf("\nOrder #1");
+    printf("\nMenu  : ");
+
+    printf("\nDrink : ");
+
+    printf("\n\n---------------------------");
+    printf("\nCustomer name: ");
+
+    printf("\nDine in.");
+    printf("\nTake away.");
+
+    printf("\n\nTotal to pay is: RM");
+    printf("\nEnjoy your meal!\n");
+
+return 0;
+}
