f5b2dd9a5ff97e4c9b48e1c7af13e32b1dde339a
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-09-01 19:16:27 +0800

    [HOT1: 2020(2)] Update priceTotal formula
    Update priceTotal formula by adding priceFood, priceDrink and pricePlastic.

--- hot01_2020(2).c
+++ hot01_2020(2).c
@@ -5,7 +5,7 @@ int main(void){
 
     char custName[50], orderFood[5], orderDrink[5], serviceMode[50];
     int loop, orderTotal, orderType, orderPlastic;
-    float priceTotal;
+    float priceTotal=0, priceFood, priceDrink, pricePlastic=0;
 
     printf("Welcome to Nasi Kukus MyAbah Restaurant");
 
@@ -39,26 +39,68 @@ int main(void){
 
         strcpy(serviceMode, "Takeaway");
         printf("Need plastic bag? (1=Yes, 0=No)       : ");
-        scanf(" %d", &orderPlastic);}
+        scanf(" %d", &orderPlastic);
+
+        if(orderPlastic==1){
+            pricePlastic=0.20;}}
 
     else{
         strcpy(serviceMode, "Dine-in");}
 
     for(loop=0; loop<orderTotal; loop++){
+
         printf("\nOrder #%d", loop+1);
 
         printf("\nMenu  : ");
         scanf(" %s", &orderFood);
 
         printf("Drink : ");
-        scanf(" %s", &orderDrink);}
+        scanf(" %s", &orderDrink);
+
+        if(strcasecmp(orderFood, "AR") == 0){
+            priceFood=7.2;}
+
+        else if(strcasecmp(orderFood, "AC") == 0){
+            priceFood=7.5;}
+
+        else if(strcasecmp(orderFood, "AK") == 0){
+            priceFood=7.6;}
+
+        else if(strcasecmp(orderFood, "AH") == 0){
+            priceFood=7.6;}
+
+        else if(strcasecmp(orderFood, "AP") == 0){
+            priceFood=8.5;}
+
+        else if(strcasecmp(orderFood, "AM") == 0){
+            priceFood=8.0;}
+
+        else{
+            printf("\nMENU NOT AVAILABLE");}
+
+        if(strcasecmp(orderDrink, "DSL") == 0){
+            priceDrink=2.5;}
+
+        else if(strcasecmp(orderDrink, "DTL") == 0){
+            priceDrink=2.6;}
+
+        else if(strcasecmp(orderDrink, "DSR") == 0){
+            priceDrink=2.3;}
+
+        else if(strcasecmp(orderDrink, "DAM") == 0){
+            priceDrink=1.5;}
+
+        else{
+            printf("\nDRINK NOT AVAILABLE");}
+
+        priceTotal+=priceFood+priceDrink;}
 
     printf("\n---------------------------");
     printf("\nCustomer name: %s", custName);
 
     printf("\n%s.", serviceMode);
 
-    printf("\n\nTotal to pay is: RM%.2f", priceTotal);
+    printf("\n\nTotal to pay is: RM%.2f", priceTotal+pricePlastic);
     printf("\nEnjoy your meal!\n");
 
 return 0;
