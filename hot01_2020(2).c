ed8701ff4cd60768983316302ea113ab7edd6dfe
Zhafran Zainal <zhafranzainal11@gmail.com>
2021-09-01 19:20:08 +0800

    [HOT1: 2020(2)] Display all orders
    Display all menu and drink ordered by customer.

--- hot01_2020(2).c
+++ hot01_2020(2).c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX 10
+#define LENGTH 50
+
 int main(void){
 
-    char custName[50], orderFood[5], orderDrink[5], serviceMode[50];
+    char custName[50], orderFood[MAX][LENGTH], orderDrink[MAX][LENGTH], serviceMode[50], infoFood[MAX][LENGTH], infoDrink[MAX][LENGTH];
     int loop, orderTotal, orderType, orderPlastic;
     float priceTotal=0, priceFood, priceDrink, pricePlastic=0;
 
@@ -59,43 +62,53 @@ int main(void){
         printf("\nOrder #%d", loop+1);
 
         printf("\nMenu  : ");
-        scanf(" %s", &orderFood);
+        scanf(" %s", &orderFood[loop]);
 
         printf("Drink : ");
-        scanf(" %s", &orderDrink);
+        scanf(" %s", &orderDrink[loop]);
 
-        if(strcasecmp(orderFood, "AR") == 0){
-            priceFood=7.2;}
+        if(strcasecmp(orderFood[loop], "AR") == 0){
+            priceFood=7.2;
+            strcpy(infoFood[loop], "Nasi + Ayam goreng berempah");}
 
-        else if(strcasecmp(orderFood, "AC") == 0){
-            priceFood=7.5;}
+        else if(strcasecmp(orderFood[loop], "AC") == 0){
+            priceFood=7.5;
+            strcpy(infoFood[loop], "Nasi + Ayam masak kicap");}
 
-        else if(strcasecmp(orderFood, "AK") == 0){
-            priceFood=7.6;}
+        else if(strcasecmp(orderFood[loop], "AK") == 0){
+            priceFood=7.6;
+            strcpy(infoFood[loop], "Nasi + Ayam masak kari");}
 
-        else if(strcasecmp(orderFood, "AH") == 0){
-            priceFood=7.6;}
+        else if(strcasecmp(orderFood[loop], "AH") == 0){
+            priceFood=7.6;
+            strcpy(infoFood[loop], "Nasi + Ayam masak merah");}
 
-        else if(strcasecmp(orderFood, "AP") == 0){
-            priceFood=8.5;}
+        else if(strcasecmp(orderFood[loop], "AP") == 0){
+            priceFood=8.5;
+            strcpy(infoFood[loop], "Nasi + Ayam percik");}
 
-        else if(strcasecmp(orderFood, "AM") == 0){
-            priceFood=8.0;}
+        else if(strcasecmp(orderFood[loop], "AM") == 0){
+            priceFood=8.0;
+            strcpy(infoFood[loop], "Nasi + Ayam madu");}
 
         else{
             printf("\nMENU NOT AVAILABLE");}
 
-        if(strcasecmp(orderDrink, "DSL") == 0){
-            priceDrink=2.5;}
+        if(strcasecmp(orderDrink[loop], "DSL") == 0){
+            priceDrink=2.5;
+            strcpy(infoDrink[loop], "Sirap Limau");}
 
-        else if(strcasecmp(orderDrink, "DTL") == 0){
-            priceDrink=2.6;}
+        else if(strcasecmp(orderDrink[loop], "DTL") == 0){
+            priceDrink=2.6;
+            strcpy(infoDrink[loop], "Teh O Limau");}
 
-        else if(strcasecmp(orderDrink, "DSR") == 0){
-            priceDrink=2.3;}
+        else if(strcasecmp(orderDrink[loop], "DSR") == 0){
+            priceDrink=2.3;
+            strcpy(infoDrink[loop], "Sarsi");}
 
-        else if(strcasecmp(orderDrink, "DAM") == 0){
-            priceDrink=1.5;}
+        else if(strcasecmp(orderDrink[loop], "DAM") == 0){
+            priceDrink=1.5;
+            strcpy(infoDrink[loop], "Air Mineral");}
 
         else{
             printf("\nDRINK NOT AVAILABLE");}
@@ -105,7 +118,11 @@ int main(void){
     printf("\n---------------------------");
     printf("\nCustomer name: %s", custName);
 
-    printf("\n%s.", serviceMode);
+    printf("\n%s.\n", serviceMode);
+
+    for(loop=0; loop<orderTotal; loop++){
+        printf("\n%d) %s", loop+1, infoFood[loop]);
+        printf(" with %s", infoDrink[loop]);}
 
     printf("\n\nTotal to pay is: RM%.2f", priceTotal+pricePlastic);
     printf("\nEnjoy your meal!\n");
