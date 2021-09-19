cbd73faf4f0de088cfc037c7f110e90f4697ee1d
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-09-20 00:26:23 +0800

    [HOT1: 2021(1)] Create hot01_2021(1).c
    Expected output as the main structure.

---
+++ hot01_2021(1).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("\n Via Cash Back Channel? [Y=Yes, N=No]     : ");
+    printf("\n Enter shop [S=Shop Pi, F=Fashion Balik]  : ");
+
+    printf("\n\n -------------Items-------------");
+    printf("\n A: Sakura Dining Set");
+    printf("\n B: Organic Cotton Bedding Set");
+    printf("\n C: Smart TV");
+    printf("\n -------------------------------");
+    printf("\n Enter code of item to be purchased   : ");
+    printf("\n Enter unit of purchased item         : ");
+
+    printf("\n\n Use 17600.00 your coin for a RM17.60 discount and 4.50 cashback for this purchasing? [Y=Yes, N=No]: ");
+    printf("\n Not applicable for collecting coins and cashback\n");
+
+    printf("\n Shopping channel and shop    : Cash Back Channel/Shop Pi");
+    printf("\n Purchased item               : Organic Cotton Bedding Set");
+    printf("\n Number of unit               : 4");
+    printf("\n Total price             (RM) : 2200.00");
+    printf("\n Go Green Discount       (RM) : 330.00");
+    printf("\n Final price             (RM) : 1847.90");
+    printf("\n Final price             (B$) : 601.92");
+
+    printf("\n\n Continue? [1=Next user, 2=Exit]          : ");
+
+    printf("\n");
+
+return 0;
+}
