df7b690b5bf7d226bc493257fbc3219285febb0b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-09-20 19:31:28 +0800

    [HOT2: 2019] Determine data type and variable name
    Determine data type and variable name before variable declaration.

--- hot02_2019.c
+++ hot02_2019.c
@@ -8,21 +8,27 @@ int main(void){
     printf("\n----------------------------------------------------");
 
     printf("\nWhich month order? ");
-    printf("\nHow many orders for month March? ");
+    scanf(" %s", &orderMonth);
+
+    printf("\nHow many orders for month %s? ", orderMonth);
+    scanf(" %d", &orderTotal);
 
     printf("\n\nCustomer 1 name: ");
-    printf("\nTart Nenas, Kuih Kapit and Tart Coklat orders (separate each input by space) :\n");
+    scanf(" %[^\n]s", &custName);
 
-    printf("\nOrder for month : March");
+    printf("\nTart Nenas, Kuih Kapit and Tart Coklat orders (separate each input by space) :\n");
+    scanf("%d %d %d", &cookieTN, &cookieKK, &cookieTC);
+
+    printf("\nOrder for month : %s", orderMonth);
     printf("\nCustomer\t Tart Nenas\t Kuih Kapit\t Tart Coklat\t Payment(RM)");
 
-    printf("\nCik Mah\t\t 2\t\t 1\t\t 2\t\t 134.00");
+    printf("\n%-20s %-15d %-15d %-15d %.2f", custName, cookieTN, cookieKK, cookieTC, payment);
 
     printf("\n\n========================================");
-    printf("\nTotal Tart Nenas  : ");
-    printf("\nTotal Kuih Kapit  : ");
-    printf("\nTotal Tart Coklat : ");
-    printf("\nTotal Profit      : RM");
+    printf("\nTotal Tart Nenas  : %d", cookieTN_all);
+    printf("\nTotal Kuih Kapit  : %d", cookieKK_all);
+    printf("\nTotal Tart Coklat : %d", cookieTC_all);
+    printf("\nTotal Profit      : RM%.2f", totalProfit);
     printf("\n\n========================================");
 
 return 0;
