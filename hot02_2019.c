789dfbc78a12e9d59de84dc9e6725217f6c9e060
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-09-20 19:27:04 +0800

    [HOT2: 2019] Create hot02_2019.c
    Expected output as the main structure.

---
+++ hot02_2019.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("\tVIRAL COOKIES COMPANY");
+    printf("\n\tMONTHLY COOKIES ORDER REPORT");
+    printf("\n\tTART NENAS || KUIH KAPIT || TART COKLAT");
+    printf("\n----------------------------------------------------");
+
+    printf("\nWhich month order? ");
+    printf("\nHow many orders for month March? ");
+
+    printf("\n\nCustomer 1 name: ");
+    printf("\nTart Nenas, Kuih Kapit and Tart Coklat orders (separate each input by space) :\n");
+
+    printf("\nOrder for month : March");
+    printf("\nCustomer\t Tart Nenas\t Kuih Kapit\t Tart Coklat\t Payment(RM)");
+
+    printf("\nCik Mah\t\t 2\t\t 1\t\t 2\t\t 134.00");
+
+    printf("\n\n========================================");
+    printf("\nTotal Tart Nenas  : ");
+    printf("\nTotal Kuih Kapit  : ");
+    printf("\nTotal Tart Coklat : ");
+    printf("\nTotal Profit      : RM");
+    printf("\n\n========================================");
+
+return 0;
+}
