eb3b311218d051fc85cc452e1f668fd62edf1e8c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-09-25 18:41:46 +0800

    [HOT2: 2020] Determine data type and variable name
    Determine data type and variable name before variable declaration.

--- hot02_2020.c
+++ hot02_2020.c
@@ -3,30 +3,40 @@
 int main(void){
 
     printf(" How many customer to enter? ");
+    scanf(" %d", &custTotal);
 
     printf("\n\n Enter rider name                     : ");
+    gets(riderName);
+
     printf("\n Enter rider ID                       : ");
+    gets(riderID);
 
     printf("\n\n Customer #1");
-    printf("\n Enter pick up time (24hrs format)    : ");
-    printf("\n Enter distance                       : ");
-    printf("\n Enter food price                     : ");
 
-    printf("\n\n Name        : ");
-    printf("\n Rider ID    : ");
+    printf("\n Enter pick up time (24hrs format)    : ");
+    scanf(" %d", &orderTime);
+
+    printf("\n Enter distance                       : ");
+    scanf(" %d", &orderDistance);
+
+    printf("\n Enter food price                     : ");
+    scanf(" %f", &orderPrice);
+
+    printf("\n\n Name        : %s", riderName);
+    printf("\n Rider ID    : %s", riderID);
 
     printf("\n\n Delivery Information and Details");
     printf("\n ================================");
 
     printf("\n\n Time  Distance Food Price COD Charge Price Disc %% New Price Pay to Rider");
     printf("\n ----- -------- ---------- ---------- ----- ------ --------- -------------");
-    printf("\n 0945     24      17.50      12.00    29.50   5.0    28.02       6.08");
+    printf("\n %04d     %d      %.2f      %.2f    %.2f   %.1f    %.2f       %.2f", orderTime, orderDistance, orderPrice, priceCOD, priceOld, discount, priceNew, riderPayment);
 
     printf("\n\n Overall Information");
     printf("\n ===================");
-    printf("\n Total New Price       : RM");
-    printf("\n Total Pay to Rider    : RM");
-    printf("\n Number of Bonus Point : ");
+    printf("\n Total New Price       : RM%.2f", totalPrice);
+    printf("\n Total Pay to Rider    : RM%.2f", totalPayment);
+    printf("\n Number of Bonus Point : %.0f", totalPrice);
 
     printf("\n");
 
