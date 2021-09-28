72446bc5e6e7b1eb594a7dd45decbc634345014a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-09-28 18:29:07 +0800

    [HOT2: 2021(1)] Automate calculation for summonLate
    Automate calculation for number of days late by calculating difference between summon date issued and current date.

--- hot02_2021(1).c
+++ hot02_2021(1).c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <time.h>
+
+typedef struct Date{
+    int days, months, years;
+    }Date;
+
+//Function Declaration (Prototype)
+int calc_dateDifference(Date dateSummon, Date dateCurrent);
+int countLeapYears(Date dateLeap);
 
 int main(void){
 
     float summonAmount, summonTotal, summonDiscount, summonPenalty;
-    char driverName[50], driverID[20], driverPlat[10], dateSummon[20];
+    char driverName[50], driverID[20], driverPlat[10];
     int summonLate;
 
     printf("Enter driver name     : ");
@@ -20,13 +29,24 @@ int main(void){
 
     fflush(stdin);
 
-    printf("Date of summon issued : ");
-    gets(dateSummon);
+    struct Date dateSummon;
+
+    printf("\nDate of summon issued \n(DD/MM/YYYY)          : ");
+    scanf("%d/%d/%d", &dateSummon.days, &dateSummon.months, &dateSummon.years);
+
+    //currentDate
+    time_t t;
+    t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    Date dateCurrent = {tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900};
 
     printf("\nThe summon needs to be paid within 5 days from the date of summon issued.");
 
-    printf("\n\nNo of days late       : ");
-    scanf(" %d", &summonLate);
+    //Function Calling: calc_dateDifference
+    summonLate=calc_dateDifference(dateSummon, dateCurrent);
+
+    printf("\n\nNo of days late is %d\n", summonLate);
 
     summonDiscount=0;
 
@@ -66,3 +86,52 @@ int main(void){
 
 return 0;
 }
+
+//Function Definition: calc_dateDifference
+//This function returns number of days between two given dates
+int calc_dateDifference(Date dateSummon, Date dateCurrent){
+
+    int loop;
+
+    //To store number of days in all months from January to December
+    const int monthDays[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+
+    /**COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dateSummon'**/
+    //initialize count using years and day
+    long int dateFirst=(dateSummon.years*365)+dateSummon.days;
+
+    //Add days for months in given date
+    for(loop=0; loop<dateSummon.months-1; loop++){
+        dateFirst+=monthDays[loop];}
+
+    //Since every leap year is of 366 days, add a day for every leap year
+    //Function Calling: countLeapYears
+    dateFirst+=countLeapYears(dateSummon);
+
+    /**COUNT TOTAL NUMBER OF DAYS BEFORE SECOND DATE 'dateCurrent'**/
+    //initialize count using years and day
+    long int dateSecond=(dateCurrent.years*365)+dateCurrent.days;
+
+    //Add days for months in given date
+    for(loop=0; loop<dateCurrent.months-1; loop++){
+        dateSecond+=monthDays[loop];}
+
+    //Since every leap year is of 366 days, add a day for every leap year
+    //Function Calling: countLeapYears
+    dateSecond+=countLeapYears(dateCurrent);
+
+    //return difference between two counts
+    return (dateSecond-dateFirst);}
+
+//Function Definition: countLeapYears
+//This function counts number of leap years before the given date
+int countLeapYears(Date dateLeap){
+
+    int yearLeap=dateLeap.years;
+
+    //Check if the current year needs to be considered for the count of leap years or not
+    if(dateLeap.months<=2){
+        yearLeap--;}
+
+    //A year is a leap year if it is a multiple of 4 or multiple of 400 and not a multiple of 100.
+    return (yearLeap/4)+(yearLeap/400)-(yearLeap/100);}
