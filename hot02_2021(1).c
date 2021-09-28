c47afec7ba32d06adc9a30d04181a9a3ba0caf8b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-09-28 18:10:27 +0800

    [HOT2: 2021(1)] Create hot02_2021(1).c
    Expected output as the main structure.

---
+++ hot02_2021(1).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Enter driver name     : ");
+    printf("\nEnter ID no           : ");
+    printf("\nEnter vehicle no      : ");
+    printf("\nSummon amount (in RM) : ");
+    printf("\nDate of summon issued : ");
+
+    printf("\n\nThe summon needs to be paid within 5 days from the date of summon issued.");
+
+    printf("\n\nNo of days late       : ");
+
+    printf("\n\n---------------------------\n");
+
+    printf("\nPayment Information");
+    printf("\n===================");
+    printf("\nName                      : ");
+    printf("\nID No                     : ");
+    printf("\nVehicle No                : ");
+    printf("\nTotal Summon              : RM");
+    printf("\nDiscount                  : RM");
+    printf("\nTotal amount of penalty   : RM");
+
+    printf("\n");
+
+return 0;
+}
