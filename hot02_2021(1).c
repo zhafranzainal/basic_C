062830fdb4b7641ede57122d5b2959c0f1052e7b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-09-28 18:12:18 +0800

    [HOT2: 2021(1)] Determine data type and variable name
    Determine data type and variable name before variable declaration.

--- hot02_2021(1).c
+++ hot02_2021(1).c
@@ -3,25 +3,35 @@
 int main(void){
 
     printf("Enter driver name     : ");
+    gets(driverName);
+
     printf("\nEnter ID no           : ");
+    gets(driverID);
+
     printf("\nEnter vehicle no      : ");
+    gets(driverPlat);
+
     printf("\nSummon amount (in RM) : ");
+    scanf(" %f", &summonAmount);
+
     printf("\nDate of summon issued : ");
+    gets(dateSummon);
 
     printf("\n\nThe summon needs to be paid within 5 days from the date of summon issued.");
 
     printf("\n\nNo of days late       : ");
+    scanf(" %d", &summonLate);
 
     printf("\n\n---------------------------\n");
 
     printf("\nPayment Information");
     printf("\n===================");
-    printf("\nName                      : ");
-    printf("\nID No                     : ");
-    printf("\nVehicle No                : ");
-    printf("\nTotal Summon              : RM");
-    printf("\nDiscount                  : RM");
-    printf("\nTotal amount of penalty   : RM");
+    printf("\nName                      : %s", driverName);
+    printf("\nID No                     : %s", driverID);
+    printf("\nVehicle No                : %s", driverPlat);
+    printf("\nTotal Summon              : RM%.2f", summonTotal);
+    printf("\nDiscount                  : RM%.2f", summonDiscount);
+    printf("\nTotal amount of penalty   : RM%.2f", summonPenalty);
 
     printf("\n");
 
