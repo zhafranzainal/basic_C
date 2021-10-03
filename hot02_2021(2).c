d4561ee87416108ae042434d26b314f4d76ca5e0
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-10-03 20:46:56 +0800

    [HOT2: 2021(2)] Create hot02_2021(2).c
    Expected output as the main structure.

---
+++ hot02_2021(2).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Participant name  : ");
+    printf("\nParticipant no    : ");
+    printf("\nParticipant team  : ");
+
+    printf("\n\nJumping information (in meter):-\n");
+    printf("        Attempt 1 = ");
+
+    printf("\n\n---------- Result information ----------");
+
+    printf("\n\nParticipant Information");
+    printf("\nName  : ");
+    printf("\nNo.   : ");
+    printf("\nTeam  : ");
+
+    printf("\n\nJumping Information");
+    printf("\nAttempt 1 = 5.25 meter");
+
+    printf("\n\nLongest jump = 5.25 meter");
+    printf("\nAverage jump = 5.19 meter");
+
+    printf("\n");
+
+return 0;
+}
