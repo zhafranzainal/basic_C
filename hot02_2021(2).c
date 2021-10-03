4338275fb18725f93f40d3d907d5931054474599
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2021-10-03 20:49:14 +0800

    [HOT2: 2021(2)] Determine data type and variable name
    Determine data type and variable name before variable declaration.

--- hot02_2021(2).c
+++ hot02_2021(2).c
@@ -3,24 +3,30 @@
 int main(void){
 
     printf("Participant name  : ");
+    gets(memberName);
+
     printf("\nParticipant no    : ");
+    gets(memberNo);
+
     printf("\nParticipant team  : ");
+    gets(memberTeam);
 
     printf("\n\nJumping information (in meter):-\n");
     printf("        Attempt 1 = ");
+    scanf(" %f", &jumpAttempt);
 
     printf("\n\n---------- Result information ----------");
 
     printf("\n\nParticipant Information");
-    printf("\nName  : ");
-    printf("\nNo.   : ");
-    printf("\nTeam  : ");
+    printf("\nName  : %s", memberName);
+    printf("\nNo.   : %s", memberNo);
+    printf("\nTeam  : %s", memberTeam);
 
     printf("\n\nJumping Information");
-    printf("\nAttempt 1 = 5.25 meter");
+    printf("\nAttempt 1 = %.2f meter", jumpAttempt);
 
-    printf("\n\nLongest jump = 5.25 meter");
-    printf("\nAverage jump = 5.19 meter");
+    printf("\n\nLongest jump = %.2f meter", jumpAttempt);
+    printf("\nAverage jump = %.2f meter", jumpAverage);
 
     printf("\n");
 
